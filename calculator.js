573a77e36ad9775d66b2d460f4da89946d2c683a
unknownskull313 <numberll23ll@gmail.com>
2024-11-21 19:40:08 +0200

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+function calculate (operation) {
+  const num1 = parseFloat (document.getElementById('num1').value)
+  const num2 = parseFloat (document.getElementById('num2').value)
+  let result;
+
+
+ switch (operation) {
+  case '+' : result = num1 + num2;break;
+  case '-' : result = num1 - num2;break;
+  case '*' : result = num1 * num2;break;
+  case '/' : if (num2 ===0) {alert("canot devide by zero!!");return;}  result = num1 / num2;break;
+}
+document.getElementById("result").textContent = result;
+}
